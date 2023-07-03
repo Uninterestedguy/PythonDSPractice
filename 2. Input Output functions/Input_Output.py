bf5574ef124a55d29954ca883ff87c45d3dc629c
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-07-03 19:11:33 +0530

    Create Input_output.py

---
+++ 2. Input Output functions/Input_Output.py
@@ -0,0 +1,12 @@
+# Input function
+
+num = input("Enter your number")
+
+# Output function
+
+# Sep 
+print("This","is","Separate",sep="-")
+
+# End for continuation
+print("This is 1 line,", end=" ")
+print("This is continuation of 1 line")
