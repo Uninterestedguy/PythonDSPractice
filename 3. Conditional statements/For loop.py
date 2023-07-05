534cb9aba8dad30f5d6ae46e7c48442dfcbd1f35
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-07-05 21:05:43 +0530

    Create For loop.py

---
+++ 3. Conditional statements/For loop.py
@@ -0,0 +1,16 @@
+# For loop 
+
+# For loop require a sequence data type for iteration
+
+for i in range(0,8):
+	print(" This is range iteration")
+
+List= [1,2,3,4,5,6]
+
+for i in List:
+	print(" This is list iteration")
+
+str1 = "This is string iteration"
+
+for i in str1:
+	print(i, ends=" ")
